2742be774a1d80bc29f515580315f993c2889841
helio.terashima <helio.terashima@netconamericas.com>
2024-11-08 17:30:00 -0300

    Update

--- src/Commands/AddPackage.php
+++ src/Commands/AddPackage.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Naoray\LaravelPackageMaker\Commands;
+namespace Hesete\LaravelPackageMaker\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
-use Naoray\LaravelPackageMaker\Traits\InteractsWithTerminal;
+use Hesete\LaravelPackageMaker\Traits\InteractsWithTerminal;
 
 class AddPackage extends Command
 {
